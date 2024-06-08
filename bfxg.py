88e404de53adfac5d3871d3fd9a382c94184160a
MarcelSuleiman <marcelsuleiman@gmail.com>
2024-06-08 22:19:51 +0200

    remove secrets from client

--- bfxg.py
+++ bfxg.py
@@ -28,10 +28,13 @@ ticker_vocabulary = {
 	15: "FRR_AMOUNT_AVAILABLE"
 }
 
-class BitfinexClient(object):
+
+class BitfinexClient:
 	BASE_URL = "https://api.bitfinex.com/"
-	KEY = "" # <- from your account
-	SECRET = "" # <- from your account
+
+	def __init__(self, key: str, secret: str):
+		self.key = key
+		self.secret = secret
 
 	def _nonce(self):
 		# Returns a nonce
@@ -39,27 +42,27 @@ class BitfinexClient(object):
 		return str(int(round(time.time() * 10000)))
 
 	def _headers(self, path, nonce, body):
-		secbytes = self.SECRET.encode(encoding='UTF-8')
+		secbytes = self.secret.encode(encoding='UTF-8')
 		signature = "/api/" + path + nonce + body
 		sigbytes = signature.encode(encoding='UTF-8')
 		h = hmac.new(secbytes, sigbytes, hashlib.sha384)
 		hexstring = h.hexdigest()
 		return {
 			"bfx-nonce": nonce,
-			"bfx-apikey": self.KEY,
+			"bfx-apikey": self.key,
 			"bfx-signature": hexstring,
 			"content-type": "application/json",
 		}
 
-	def req(self, path, params = {}, payloads = {}):
+	def req(self, path, params={}, payloads={}):
 		nonce = self._nonce()
 		body = params
-		rawBody = json.dumps(body)
-		headers = self._headers(path, nonce, rawBody)
+		raw_body = json.dumps(body)
+		headers = self._headers(path, nonce, raw_body)
 		url = self.BASE_URL + path
 		while True:
 			try:
-				resp = requests.post(url, json=payloads, headers=headers, data=rawBody, verify=True)
+				resp = requests.post(url, json=payloads, headers=headers, data=raw_body, verify=True)
 				break
 			except Exception as e:
 				print(f"{e.__class__.__name__} - {str(e)}")
@@ -180,71 +183,9 @@ class BitfinexClient(object):
 
 if __name__ == "__main__":
 
-
+	key = ""
+	secret = ""
 
 	# fetch all your orders and print out
-	client = BitfinexClient()
-	# result = client.active_orders()
-	# print(result)
-
-	result = client.set_cancel_funding_order(_id=2505835328) # WTF? what's means?
-
-	# result = client.active_funding_orders()
-	# print(result)
-
-	# for r in result:
-	# 	print(r)
-
-	# result = client.history_funding_orders()
-	# for r in result:
-	# 	print(r)
-
-	# result = client.set_funding_order("LIMIT", "fLTC", "2.55", "0.0006", 30, flags = 0 )
-	# print(result)
-
-	# result = client.get_funding_statistics("fUSD")
-	# print(result)
-
-	# result = client.get_ticker_statistics("fUSD")
-	# for i, r in enumerate(result):
-	# 	print(f"{ticker_vocabulary[i]}: {r}")
-
-	# while True:
-
-	# 	result = client.get_wallets()
-	# 	# print(result)
-	# 	for r in result:
-	# 		if r[0] == "funding" and r[1] == "USD":
-	# 			# print(r[4])
-	# 			available_usd_balance = r[4]
-	# 			print(f"Dostupný zostatok na účte je: {round(available_usd_balance, 2)} $")
-
-	# 			if available_usd_balance >= 150:
-	# 			# if available_usd_balance < 150:
-	# 				ticker_stat = client.get_ticker_statistics("fUSD")
-	# 				daily_high = ticker_stat[11]
-	# 				print(daily_high)
-
-	# 				# my_rate = daily_high - 0.00002 # test, v skutocnosti by to mal byt -
-	# 				my_rate = daily_high + 0.00005 # test, v skutocnosti by to mal byt -
-
-	# 				print(my_rate)
-	# 				print(round(my_rate, 6))
-
-	# 				_type = "LIMIT"
-	# 				symbol = "fUSD"
-	# 				amount = available_usd_balance
-	# 				rate = round(my_rate, 6)
-
-	# 				if my_rate > 0.00065:
-	# 					period = 30
-	# 				else:
-	# 					period = 2
-
-	# 				if my_rate > MAX_RATE:
-	# 					my_rate = MAX_RATE - 0.00002
-
-	# 				resp = client.set_funding_order(_type, symbol, amount, rate, period, flags = 0)
-	# 				print(resp)
-
-	# 	sleep(60)
+	client = BitfinexClient(key=key, secret=secret)
+	result = client.set_cancel_funding_order(_id=2505835328)
